c3003880296af675937900e6bcb3e72491d9bdde
Manasvi <gehlotmanasvi6@gmail.com>
2026-06-05 21:36:04 +0530

    fixed css

--- personal-finance-app/src/App.jsx
+++ personal-finance-app/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import './components/Sidebar.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import Searchbar from './components/Searchbar.jsx'
 import {Outlet} from 'react-router-dom'
@@ -12,8 +10,8 @@ function App() {
         <Sidebar />
         <Searchbar />
       </div>
-      <main className="ml-70 mt-16 mb-0 min-h-screen bg-[#0a0e14] p-6 text-white">
-        <Outlet/>
+      <main className="ml-70 mt-16 mb-0 min-h-[calc(100vh-4rem)] bg-[#0a0e14] p-6 text-white">
+        <Outlet />
       </main>
     </>
   )
